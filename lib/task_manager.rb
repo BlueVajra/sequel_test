d3a6aab95b4984d7d6561d1c534da630befc9aff
Cory Leistikow <cory.leistikow@gmail.com>
2014-04-03 17:52:17 -0600

    user can update and delete tasks

--- lib/task_manager.rb
+++ lib/task_manager.rb
@@ -5,17 +5,27 @@ class TaskManager
     @db = db
     @db.create_table! :tasks do
       primary_key :id
+      Boolean :completed
       String :name
     end
-
     @tasks = @db[:tasks]
   end
 
   def add_task(task)
-    @tasks.insert(task)
+    incoming_task = task
+    incoming_task[:completed] = false
+    @tasks.insert(incoming_task)
   end
 
   def show_all
     @tasks.all
+  end
+
+  def update_task(task)
+    @tasks.where('id = ' + task[:id].to_s).update(task)
+  end
+
+  def delete_task(task)
+    @tasks.where('id = ' + task[:id].to_s).delete
   end
 end
--- spec/task_manager_spec.rb
+++ spec/task_manager_spec.rb
@@ -1,13 +1,53 @@
 require 'task_manager'
 
 describe TaskManager do
+  before(:each) do
+    @db = Sequel.connect('postgres://gschool_user:password@localhost:5432/sequel_test')
+  end
+
   it "adds multiple tasks and returns an array of all tasks" do
-    #add a task
-    db = Sequel.connect('postgres://gschool_user:password@localhost:5432/sequel_test')
-    tasks = TaskManager.new(db)
+    tasks = TaskManager.new(@db)
     tasks.add_task({name: "Do One Task"})
     tasks.add_task({name: "Do A Second Task"})
-    expected = [{:id=>1, :name=>"Do One Task"}, {:id=>2, :name=>"Do A Second Task"}]
+    expected = [{:id => 1, :name => "Do One Task", :completed => false}, {:id => 2, :name => "Do A Second Task", :completed => false}]
     expect(tasks.show_all).to eq(expected)
   end
+
+  it "completes a task" do
+    tasks = TaskManager.new(@db)
+    tasks.add_task({name: "Do One Task"})
+    tasks.add_task({name: "Do Another Task"})
+    tasks.update_task({:id => 2, :completed => true})
+
+    actual = tasks.show_all
+    expected = [{:id => 1, :name => "Do One Task", :completed => false},
+                {:id => 2, :name => "Do Another Task", :completed => true}]
+
+    expect(actual).to eq(expected)
+  end
+
+  it "completes and updates a task" do
+    tasks = TaskManager.new(@db)
+    tasks.add_task({name: "Do One Task"})
+    tasks.add_task({name: "Do Another Task"})
+    tasks.update_task({:id => 2, :completed => true, name: "Finished Task"})
+
+    actual = tasks.show_all
+    expected = [{:id => 1, :name => "Do One Task", :completed => false},
+                {:id => 2, :name => "Finished Task", :completed => true}]
+
+    expect(actual).to eq(expected)
+  end
+
+  it "deletes a task" do
+    tasks = TaskManager.new(@db)
+    tasks.add_task({name: "Do One Task"})
+    tasks.add_task({name: "Do Another Task"})
+    tasks.delete_task({id: 1})
+    actual = tasks.show_all
+    expected = [{:id => 2, :name => "Do Another Task", :completed => false}]
+    expect(actual).to eq(expected)
+  end
+
+
 end
